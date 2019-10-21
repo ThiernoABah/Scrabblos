5ffa66274d5674242569ffdcf9ad7e5195c66796
root <kev.erdogan@gmail.com>
2019-10-21 23:39:51 +0200

    Encodage et envoie de message au serveur

--- src/fr/scrabblos/Author.java
+++ src/fr/scrabblos/Author.java
@@ -1,5 +1,10 @@
 package fr.scrabblos;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.KeyPair;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -8,28 +13,30 @@ import org.bouncycastle.util.encoders.Hex;
 
 public class Author implements Runnable {
 
-	private int privateKey;
-	private int publicKey;
+	private KeyPair pair;
 	private ArrayList<Character> bag=null;
 	private Socket socket;
+	private PrintWriter writer;
+	private BufferedReader reader;
 	
-	public Author(Socket s) {
-		this.socket = s;
+	public Author(String host, int port) {
+		try {
+			socket = new Socket(host, port);
+			writer = new PrintWriter(socket.getOutputStream(), true);
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		pair = Utils.getNewKey("author");
 	}
 
 	@Override
 	public void run() {
-		//this.socket.connect();  Connection au serveur
+		connect();
+		getBag();
 		
-		try {
-			this.socket.wait();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		/*
 		boolean fin = false;
-		
 		while(!fin) {
 			Random r = new Random();
 			int rand = r.nextInt(bag.size());
@@ -41,13 +48,28 @@ public class Author implements Runnable {
 				e.printStackTrace();
 			}
 		}
+		*/
 		
-		
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void connect() {
-		// ici le client envoie sa clé public au serveur et recoit un sac de lettre du serveur
+	private void getBag() {
+		try {
+			String line = reader.readLine();
+			System.out.println("msg from serv : "+line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+
+	/* ici le client envoi sa clé public au serveur */
+	public void connect() {
+		sendMessage("{ \"register\" : "+Hex.toHexString(pair.getPublic().getFormat().getBytes())+" }");
+	} 
 	
 	public void receiveLetter(ArrayList<Character> sac) {
 		this.bag = sac;
@@ -67,8 +89,17 @@ public class Author implements Runnable {
 		
 	}
 	
+	public void sendMessage(String msgJson) {
+		long length = msgJson.length();
+		String msgToSend = Long.toBinaryString(length) + msgJson;
+		// send to serv
+		writer.println(msgToSend);
+	}
 	
 	
 	
-	
+	public static void main(String[] args) {
+		Author a = new Author("127.0.0.1", 12346);
+		new Thread(a).start();
+	}
 }
